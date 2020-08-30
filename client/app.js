fd202cacf2f0476453424c42e4fb818e33395885
elm-suf <elm.sufian@gmail.com>
2020-08-30 10:35:07 +0200

    add post count

--- client/app.js
+++ client/app.js
@@ -1,19 +1,33 @@
 document.getElementById("btn").onclick = (evt) => getData();
 
+function displayCount(count) {
+  document.getElementById("count-p").style.display = "block";
+  document.getElementById("count-sp").textContent = count;
+}
+
 async function getData() {
+  document.getElementById("count-p").style.display = "none";
+
   const placeId = document.getElementById("placeId").value;
   const count = document.getElementById("count").value;
   const port = 6969;
 
-  document.getElementById("loading").style.display='block'
+  document.getElementById("loading").style.display = "block";
   fetch(`/data/${placeId}/${count}`)
-    .then((response) => response.blob())
+    .then((response) => {
+      for (var pair of response.headers.entries()) {
+        if (pair[0] === "x-count") {
+          displayCount(pair[1]);
+        }
+      }
+      return response.blob();
+    })
     .then((blob) => {
       var url = window.URL.createObjectURL(blob);
       var a = document.createElement("a");
       a.href = url;
       a.download = "data.csv";
-      document.getElementById("loading").style.display='none'
+      document.getElementById("loading").style.display = "none";
 
       document.body.appendChild(a); // we need to append the element to the dom -> otherwise it will not work in firefox
       a.click();
--- index.js
+++ index.js
@@ -2,12 +2,9 @@ const express = require("express");
 const instaTouch = require("instatouch");
 
 const app = express();
-app.use(express.static('client'));
+app.use(express.static("client"));
 
 const port = process.env.PORT || 6969;
-
-const allowedOrigins = ["http://localhost:3000", "http://localhost:8080"];
-
 
 app.get("/data/:placeId/:count", async (req, res) => {
   const placeId = req.params.placeId;
@@ -16,15 +13,21 @@ app.get("/data/:placeId/:count", async (req, res) => {
 
   console.log("Pid", placeId);
   console.log("count", count);
+
   try {
     const location = await instaTouch.location(placeId, options);
+    const opt = {
+      headers: {
+        "x-count": location.count,
+      },
+    };
     // console.log("loc: " + JSON.stringify(location));
-    res.sendFile(location.csv);
+    res.sendFile(location.csv, opt);
   } catch (error) {
     res.send("sorry");
   }
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}/data`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
